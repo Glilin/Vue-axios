811c9271af8ff173da1588c686e4923fae32f15a
Glilin <1576378853@163.com>
2019-08-22 15:23:29 +0800

    axios发送get请求 没有带参数

--- components/18.axios-get.vue
+++ components/18.axios-get.vue
@@ -1,9 +1,28 @@
 <template>
-  <div></div>
+  <div class="get">
+    <p>这个文件讲解axios发起get方式的请求</p>
+    <input type="button" value="get请求" @click="getcatelist" />
+  </div>
 </template>
 
 <script>
-export default {};
+// 在当前文件目录打开Powershell 下载npm install axios 然后在app.js引入文件，最后引入当前文件
+// 引入axios
+import axios from "axios";
+export default {
+  methods: {
+    getcatelist() {
+      axios
+        .get("http://127.0.0.1:3000/getAllCateList") //返回一个promise对象
+        .then(re => {
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
